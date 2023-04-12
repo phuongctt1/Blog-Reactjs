08d16d2152baf7a1b499e4e795a27276179d492d
your name <phuong.caothithu1@gmail.com>
2023-04-12 23:09:26 +0700

    commit

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -3,26 +3,6 @@ export const nav = [
     id: 1,
     text: "home",
     url: "/",
-  },
-  {
-    id: 2,
-    text: "about me",
-    url: "/about",
-  },
-  {
-    id: 3,
-    text: "pages",
-    url: "/pages",
-  },
-  {
-    id: 4,
-    text: "blog",
-    url: "/blog",
-  },
-  {
-    id: 5,
-    text: "contact",
-    url: "/contact",
   }
 ]
 export const category = [
