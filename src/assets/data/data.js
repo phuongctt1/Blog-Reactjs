0410272d2ee59cc9e502b28066fca4b61fe7122d
your name <phuong.caothithu1@gmail.com>
2023-04-13 20:02:41 +0700

    commit3

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -10,19 +10,19 @@ export const category = [
     id: 1,
     category: "Days",
     title: "have a Day",
-    cover: "../images/category/anh1.png",
+    cover: "https://i.pinimg.com/564x/ee/f8/8d/eef88d5199d598da5dc52ec70c9431cf.jpg",
   },
   {
     id: 2,
     category: "Anime",
     title: "watch a movie",
-    cover: "../images/category/anh2.png",
+    cover: "https://i.pinimg.com/564x/87/31/92/873192e617e4e6d252371deb90594074.jpg",
   },
   {
     id: 3,
     category: "About me",
     title: "Cao Thi Thu Phuong",
-    cover: "../images/category/anh3.png",
+    cover: "https://i.pinimg.com/564x/96/bb/be/96bbbee01f7c62b34f4b6945725cd145.jpg",
   }
   
 ]
@@ -32,7 +32,7 @@ export const blog = [
     id: 1,
     title: "Jolyne Cujoh",
     desc: "Like all other members of the Joestar family, Jolyne has a small, star-shaped birthmark on the rear of her left shoulder, close to her neck. She also has an image of butterfly wings tattooed on the top of her left forearm, superimposed with a downward-pointing dagger.",
-    cover: "../images/blogs/anh1.jpg",
+    cover: "https://i.pinimg.com/564x/d8/e4/8c/d8e48c4e033295d374843b3b89d2dcda.jpg",
     category: "Anime",
     date: " 05, 2023",
   },
@@ -41,7 +41,7 @@ export const blog = [
     id: 2,
     title: "Kishibe Rohan",
     desc: "Rohan is an individual of average build and height. He has an undercut that is slicked sideways. While he regularly changes outfits, Rohan keeps his trademark headband and pen earrings in every appearance.",
-    cover: "../images/blogs/anh2.jpg",
+    cover: "https://i.pinimg.com/564x/0c/9c/1e/0c9c1ec757563644b9eacc110c33f249.jpg",
     category: "Anime",
     date: "4/ 2023",
   },
@@ -50,7 +50,7 @@ export const blog = [
     title: "Jojolion ",
     desc: "Josuke Higashikata[b] is a young man with no memory of his past or name. He is temporarily named Josuke by Yasuho after her dog. He later learns that he was originally Josefumi Kujo,[c] a friend of the Kira Family whose body was fused with Yoshikage Kira's by the Wall Eyes and a new form of Locacaca fruit. He uses the Stand Soft & Wet,[d] which allows him to produce soap bubbles that absorb an aspect of whatever object they touch, such as a person's eyesight or the friction of a floor. He later discovers another ability of his Stand he dubs Go Beyond,[e] allowing him to shoot bubbles imbued with an explosive and transcendent form of the Spin phenomenon from Steel Ball Run.",
     category: "Anime",
-    cover: "../images/blogs/anh3.jpg",
+    cover: "https://i.pinimg.com/564x/64/91/0f/64910f712c8eff47b6160c7e78315725.jpg",
     date: "5/2023",
   },
   {
@@ -58,7 +58,7 @@ export const blog = [
     title: "blog",
     desc: "Students usually start the day by getting ready for school. They wake up, brush their teeth, wash their faces and eat breakfast before getting ready for school and going to school. At school, they take classes ranging from math to literature and science. After school, they can participate in extracurricular activities or do homework.",
     category: "Days",
-    cover: "../images/blogs/anh4.jpg",
+    cover: "https://i.pinimg.com/564x/27/01/6d/27016da2725d5273ec5f7a1098cad230.jpg",
     date: "5/2023",
   },
   {
@@ -66,7 +66,7 @@ export const blog = [
     title: "April flower season in Ha Noi",
     desc: "In April, when spring is pouring in, the flowers on the streets of Hanoi begin to bloom. From the old town to the modern streets, there are beautiful flowers everywhere such as peach blossom, ban flower, milk flower and phoenix flower. Parks and gardens also receive a new color of flowers, creating a joyful, vibrant atmosphere.",
     category: "Days",
-    cover: "../images/blogs/anh5.jpg",
+    cover: "https://i.pinimg.com/564x/ef/1e/6f/ef1e6f638803b18f7022daf04822adf6.jpg",
     date: "4/ 2023",
   },
   {
@@ -74,7 +74,7 @@ export const blog = [
     title: "Profile",
     desc: "Name: Cao Thi Thu Phuong, School: Hanoi University of Science and Technology",
     category: "About me",
-    cover: "../images/blogs/anh6.jpg",
+    cover: "https://i.pinimg.com/564x/27/01/6d/27016da2725d5273ec5f7a1098cad230.jpg",
     date: "February 20, 2018",
   },
   {
@@ -82,7 +82,7 @@ export const blog = [
     title: "PicTure",
     desc: "The painting depicts a colorful natural landscape, with flower petals racing against the green grassy hills, and trees creating delicate twists on the distant hills.",
     category: "Days",
-    cover: "../images/blogs/anh7.jpg",
+    cover: "https://i.pinimg.com/564x/ef/1e/6f/ef1e6f638803b18f7022daf04822adf6.jpg",
     date: "February 20, 2018",
   },
   {
@@ -90,7 +90,7 @@ export const blog = [
     title: "PicTure",
     desc: "Painting of lanterns flying in the sky. It's a beautiful scene.",
     category: "Days",
-    cover: "../images/blogs/anh8.jpg",
+    cover: "https://i.pinimg.com/564x/81/25/63/812563db58dea326f70f41d03ffb0cb2.jpg",
     date: "4/2023",
   },
   {
@@ -98,7 +98,7 @@ export const blog = [
     title: "Pdandelion flowers in Hanoi",
     desc: "Every time the dandelion season comes, this city becomes more vibrant with thousands of beautiful purple flowers. The large and curved petals of the dandelion give off a gentle fragrance, creating a poetic and peaceful space.",
     category: "Days",
-    cover: "../images/blogs/anh9.jpg",
+    cover: "https://i.pinimg.com/564x/ff/e1/3a/ffe13a6b0edc696bf8b97ef42029f933.jpg",
     date: "4/2023",
   },
 
@@ -107,7 +107,7 @@ export const blog = [
     title: "Relax",
     desc: "The picture is beautiful, the scene is abstract, makes people relax after every stressful class",
     category: "Fashion",
-    cover: "../images/blogs/anh10.jpg",
+    cover: "https://i.pinimg.com/736x/69/65/6c/69656cd6725af11748ea6d559736b00e.jpg",
     date: "4/2023",
   },
 ]
